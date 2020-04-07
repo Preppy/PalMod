bbb05a17758185316fd23cf20ef9b2e1a874d395
Zach Robinson <meandyouftw@gmail.com>
2020-04-07 15:44:25 -0700

    General: Removed duplicated content

--- palmod2014/ShellSort.h
+++
@@ -1,5 +0,0 @@
-//#pragma once
-
-void ShellSort(double * fpSrc, double * fpExtra1, double * fpExtra2, int * pPalette, UINT16 * pTable, int max);
-void ccRGBtoXYZ(double r, double g, double b, double* x, double* y, double* z);
-void SpecialSort(double * fpSrc, int * pPalette, UINT16 * pTable, int nSize);
