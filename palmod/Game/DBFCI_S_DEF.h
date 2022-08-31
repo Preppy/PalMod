0e18c507d7204d1dde95b8c2991c8b91cc98ae10
Zach Robinson <meandyouftw@gmail.com>
2022-08-31 16:00:29 -0700

    move some data to DEF Files

---
+++ palmod/Game/DBFCI_S_DEF.h
@@ -0,0 +1,276 @@
+#pragma once
+
+const std::vector<uint16_t> DBFCI_A_IMGIDS_USED =
+{
+    indexFrenchBreadSprites_DBFCI_Akira,        // 0x00
+    indexFrenchBreadSprites_DBFCI_Ako,          // 0x01
+    indexFrenchBreadSprites_DBFCI_Asuna,        // 0x02
+    indexFrenchBreadSprites_DBFCI_Emi,          // 0x03
+    indexFrenchBreadSprites_DBFCI_Kirino,       // 0x04
+    indexFrenchBreadSprites_DBFCI_Kirito,       // 0x05
+    indexFrenchBreadSprites_DBFCI_Kuroko,       // 0x06
+    indexFrenchBreadSprites_DBFCI_Kuroyukihime, // 0x07
+    indexFrenchBreadSprites_DBFCI_Mikoto,       // 0x08
+    indexFrenchBreadSprites_DBFCI_Miyuki,       // 0x09
+    indexFrenchBreadSprites_DBFCI_Quenser,      // 0x0A
+    indexFrenchBreadSprites_DBFCI_Rentaro,      // 0x0B
+    indexFrenchBreadSprites_DBFCI_Selvaria,     // 0x0C
+    indexFrenchBreadSprites_DBFCI_Shana,        // 0x0D
+    indexFrenchBreadSprites_DBFCI_Shizuo,       // 0x0E
+    indexFrenchBreadSprites_DBFCI_Taiga,        // 0x0F
+    indexFrenchBreadSprites_DBFCI_Tatsuya,      // 0x10
+    indexFrenchBreadSprites_DBFCI_Tomoka,       // 0x11
+    indexFrenchBreadSprites_DBFCI_Yukina,       // 0x12
+    indexFrenchBreadSprites_DBFCI_Yuuki,        // 0x13
+    indexFrenchBreadSprites_DBFCI_AcceleratorLastOrder, // 0x14
+    indexFrenchBreadSprites_DBFCI_Alicia,        // 0x15
+    indexFrenchBreadSprites_DBFCI_Arita,         // 0x16
+    indexFrenchBreadSprites_DBFCI_Boogiepop,     // 0x17
+    indexFrenchBreadSprites_DBFCI_Celty,         // 0x18
+    indexFrenchBreadSprites_DBFCI_DokuroChan,    // 0x19
+    indexFrenchBreadSprites_DBFCI_Enju,          // 0x1A
+    indexFrenchBreadSprites_DBFCI_Erio,          // 0x1B
+    indexFrenchBreadSprites_DBFCI_Frolaytia,     // 0x1C
+    indexFrenchBreadSprites_DBFCI_Holo,          // 0x1D
+    indexFrenchBreadSprites_DBFCI_InnocentCharm, // 0x1E
+    indexFrenchBreadSprites_DBFCI_Izaya,         // 0x1F
+    indexFrenchBreadSprites_DBFCI_Kana,          // 0x20
+    indexFrenchBreadSprites_DBFCI_Kazari,        // 0x21
+    indexFrenchBreadSprites_DBFCI_Kino,          // 0x22
+    indexFrenchBreadSprites_DBFCI_Kojou,         // 0x23
+    indexFrenchBreadSprites_DBFCI_Kokou,         // 0x24
+    indexFrenchBreadSprites_DBFCI_Kuroneko,      // 0x25
+    indexFrenchBreadSprites_DBFCI_Leafa,         // 0x26
+    indexFrenchBreadSprites_DBFCI_Llenn,         // 0x27
+    indexFrenchBreadSprites_DBFCI_Mashiro,       // 0x28
+    indexFrenchBreadSprites_DBFCI_MiyukiAssist,  // 0x29
+    indexFrenchBreadSprites_DBFCI_PaiChan,       // 0x2A
+    indexFrenchBreadSprites_DBFCI_Rusian,        // 0x2B
+    indexFrenchBreadSprites_DBFCI_Ryuji,         // 0x2C
+    indexFrenchBreadSprites_DBFCI_Sadao,         // 0x2D
+    indexFrenchBreadSprites_DBFCI_TatsuyaAssist, // 0x2E
+    indexFrenchBreadSprites_DBFCI_Tomo,          // 0x2F
+    indexFrenchBreadSprites_DBFCI_Touma,         // 0x30
+    indexFrenchBreadSprites_DBFCI_Wilhelmina,    // 0x31
+    indexFrenchBreadSprites_DBFCI_Zero,          // 0x32
+    indexFrenchBreadSprites_DBFCI_Bonus,         // 0x33
+};
+
+struct DBFCINodeData
+{
+    LPCWSTR pszNodeName = L"uninit";
+    uint32_t nAdjustmentFromBaseNode = 0;
+};
+
+const DBFCINodeData DBFCIPaletteNodes[] =
+{
+    { L"Left",  0 },
+    { L"Right", 0x8000 },
+};
+
+const std::vector<LPCWSTR> DBFCIPaletteNamesNormal =
+{
+    L"1",
+    L"2",
+    L"3",
+    L"4",
+    L"5",
+
+    L"6",
+    L"7",
+    L"8",
+    L"9",
+    L"10",
+
+    L"11",
+    L"12",
+    L"13",
+    L"14",
+    L"15",
+
+    L"16",
+    L"17",
+    L"18",
+    L"19",
+    L"20",
+
+    L"21",
+    L"22",
+    L"23",
+    L"24",
+    L"25",
+
+    L"26",
+    L"27",
+    L"28",
+    L"29",
+    L"30",
+
+    L"31",
+    L"32",
+};
+
+// Identical for now, but leaving in so we can be flexible
+const std::vector<LPCWSTR> DBFCIPaletteNamesSupport =
+{
+    L"1",
+    L"2",
+    L"3",
+    L"4",
+    L"5",
+
+    L"6",
+    L"7",
+    L"8",
+    L"9",
+    L"10",
+
+    L"11",
+    L"12",
+    L"13",
+    L"14",
+    L"15",
+
+    L"16",
+    L"17",
+    L"18",
+    L"19",
+    L"20",
+
+    L"21",
+    L"22",
+    L"23",
+    L"24",
+    L"25",
+
+    L"26",
+    L"27",
+    L"28",
+    L"29",
+    L"30",
+
+    L"31",
+    L"32",
+};
+
+// Identical for now, but leaving in so we can be flexible
+const std::vector<LPCWSTR> DBFCIPaletteNamesEx =
+{
+    L"1",
+    L"2",
+    L"3",
+    L"4",
+    L"5",
+
+    L"6",
+    L"7",
+    L"8",
+    L"9",
+    L"10",
+
+    L"11",
+    L"12",
+    L"13",
+    L"14",
+    L"15",
+
+    L"16",
+    L"17",
+    L"18",
+    L"19",
+    L"20",
+
+    L"21",
+    L"22",
+    L"23",
+    L"24",
+    L"25",
+
+    L"26",
+    L"27",
+    L"28",
+    L"29",
+    L"30",
+
+    L"31",
+    L"32",
+};
+
+
+struct DBFCIFileData
+{
+    LPCWSTR pszFileName = nullptr;
+    LPCWSTR pszCharacter = nullptr;
+    uint32_t nExpectedFileSize = 0;
+    const std::vector<LPCWSTR> ppszPaletteList;
+    uint32_t nInitialLocation = 0;
+    uint32_t nImageUnitIndex = 0;
+    uint32_t nImagePreviewIndex = 0;
+};
+
+const DBFCIFileData DBFCICharacterData[] =
+{
+    { L"data\\_csel\\Yak.pal",    L"Akira",          65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Akira },
+    { L"data\\_csel\\Ako.pal",    L"Ako",            65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Ako },
+    { L"data\\_csel\\Asu.pal",    L"Asuna",          65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Asuna },
+    { L"data\\_csel\\Emi.pal",    L"Emi",            65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Emi },
+    { L"data\\_csel\\Krn.pal",    L"Kirino",         65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Kirino },
+    { L"data\\_csel\\Krt.pal",    L"Kirito",         65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Kirito },
+    { L"data\\_csel\\Krk.pal",    L"Kuroko",         65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Kuroko },
+    { L"data\\_csel\\Kur.pal",    L"Kuroyukihime",   65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Kuroyukihime },
+    { L"data\\_csel\\Mis.pal",    L"Mikoto",         65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Mikoto },
+    { L"data\\_csel\\Sbm.pal",    L"Miyuki",         66564,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Miyuki },
+    { L"data\\_csel\\Qen.pal",    L"Qwenthur",       65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Quenser },
+    { L"data\\_csel\\Stm.pal",    L"Rentaro",        65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Rentaro },
+    { L"data\\_csel\\Slv.pal",    L"Selvaria",       65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Selvaria },
+    { L"data\\_csel\\Sha.pal",    L"Shana",          65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Shana },
+    { L"data\\_csel\\Hiw.pal",    L"Shizuo",         65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Shizuo },
+    { L"data\\_csel\\Tgr.pal",    L"Taiga",          65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Taiga },
+    { L"data\\_csel\\Oni.pal",    L"Tatsuya",        65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Tatsuya },
+    { L"data\\_csel\\Tmk.pal",    L"Tomoka",         65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Tomoka },
+    { L"data\\_csel\\Ykn.pal",    L"Yukina",         65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Yukina },
+    { L"data\\_csel\\Yuk.pal",    L"Yuuki",          65540,    DBFCIPaletteNamesNormal,    0x4,  indexFrenchBreadSprites_DBFCI_Yuuki },
+
+    // Support characters
+    { L"data\\Acr_s_0\\Acr_s.pal",  L"Accelerator&Last Order", 65540, DBFCIPaletteNamesSupport,     0x4,  indexFrenchBreadSprites_DBFCI_Bonus, 0x00 },
+    { L"data\\Alc_s_0\\Alc_s.pal",  L"Alicia",          65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Alicia },
+    { L"data\\Har_s_0\\Har_s.pal",  L"Arita",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Arita },
+    { L"data\\Bog_s_0\\Bog_s.pal",  L"Boogiepop",       65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Boogiepop },
+    { L"data\\Crt_s_0\\Crt_s.pal",  L"Celty",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Celty },
+    { L"data\\Dok_s_0\\Dok_s.pal",  L"Dokuro-Chan",     65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_DokuroChan },
+    { L"data\\Aen_s_0\\Aen_s.pal",  L"Enju",            65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Bonus, 0x01 },
+    { L"data\\Ero_s_0\\Ero_s.pal",  L"Erio",            65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Erio },
+    { L"data\\Fro_s_0\\Fro_s.pal",  L"Frolaytia",       65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Frolaytia },
+    { L"data\\Hol_s_0\\Hol_s.pal",  L"Holo",            65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Holo },
+    { L"data\\Ino_s_0\\Ino_s.pal",  L"Innocent Charm",  65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Bonus, 0x02 },
+    { L"data\\Izy_s_0\\Izy_s.pal",  L"Izaya",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Izaya },
+    { L"data\\Iry_s_0\\Iry_s.pal",  L"Kana",            65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Kana },
+    { L"data\\Uih_s_0\\Uih_s.pal",  L"Kazari",          65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Kazari },
+    { L"data\\Kin_s_0\\Kin_s.pal",  L"Kino",            65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Kino },
+    { L"data\\Akj_s_0\\Akj_s.pal",  L"Kojou",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Kojou },
+    { L"data\\Kgk_s_0\\Kgk_s.pal",  L"Kokou",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Kokou },
+    { L"data\\Kro_s_0\\Kro_s.pal",  L"Kuroneko",        65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Bonus, 0x03 },
+    { L"data\\Lea_s_0\\Lea_s.pal",  L"Leafa",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Bonus, 0x04 },
+    { L"data\\Ren_s_0\\Ren_s.pal",  L"Llenn",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Llenn },
+    { L"data\\Mas_s_0\\Mas_s.pal",  L"Mashiro",         65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Mashiro },
+    { L"data\\Smy_s_0\\Smy_s.pal",  L"Miyuki Support",  66564,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_MiyukiAssist },
+    { L"data\\Pai_s_0\\Pai_s.pal",  L"Pai",             65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_PaiChan },
+    { L"data\\Rus_s_0\\Rus_s.pal",  L"Rusian",          65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Rusian },
+    { L"data\\Tks_s_0\\Tks_s.pal",  L"Ryuji",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Bonus, 0x05 },
+    { L"data\\Mou_s_0\\Mou_s.pal",  L"Sadao",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Sadao },
+    { L"data\\Sbt_s_0\\Sbt_s.pal",  L"Tatsuya Support", 65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_TatsuyaAssist },
+    { L"data\\Asm_s_0\\Asm_s.pal",  L"Tomo",            65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Tomo },
+    { L"data\\Tom_s_0\\Tom_s.pal",  L"Touma",           65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Touma },
+    { L"data\\Wil_s_0\\Wil_s.pal",  L"Wilhelmina",      65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Wilhelmina },
+    { L"data\\Zer_s_0\\Zer_s.pal",  L"Zero",            65540,    DBFCIPaletteNamesSupport,         0x4,  indexFrenchBreadSprites_DBFCI_Zero },
+
+    // Extra/Trump Palettes
+    { L"data\\Ako_0\\Ako_p1.pal",  L"Ako (Rusian)",  65540,    DBFCIPaletteNamesEx,             0x4,  indexFrenchBreadSprites_DBFCI_Rusian },
+    { L"data\\Emi_0\\Emi_p1.pal",  L"Emi (Trump)",   65540,    DBFCIPaletteNamesEx,             0x4,  indexFrenchBreadSprites_DBFCI_Emi },
+    { L"data\\Kur_0\\Kur_p1.pal",  L"Kuroyukihime (Red)", 65540,   DBFCIPaletteNamesEx,         0x4,  indexFrenchBreadSprites_DBFCI_Kuroyukihime },
+    { L"data\\Kur_0\\Kur_p2.pal",  L"Kuroyukihime (Green)", 65540,   DBFCIPaletteNamesEx,       0x4,  indexFrenchBreadSprites_DBFCI_Kuroyukihime },
+    { L"data\\Kur_0\\Kur_p3.pal",  L"Kuroyukihime (Blue)", 65540,   DBFCIPaletteNamesEx,        0x4,  indexFrenchBreadSprites_DBFCI_Kuroyukihime },
+    { L"data\\Stm_0\\Stm_p1.pal",  L"Rentaro (Trump)",   65540, DBFCIPaletteNamesEx,            0x4,  indexFrenchBreadSprites_DBFCI_Rentaro },
+    { L"data\\Tgr_0\\Tgr_p1.pal",  L"Taiga (Ryuji)",   65540,  DBFCIPaletteNamesEx,             0x4,  indexFrenchBreadSprites_DBFCI_Ryuji },
+    { L"data\\Oni_0\\Oni_p1.pal",  L"Tatsuya (Miyuki)", 66564, DBFCIPaletteNamesEx,             0x4,  indexFrenchBreadSprites_DBFCI_MiyukiAssist },
+};
