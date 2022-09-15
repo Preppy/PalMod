88c4a69b92178b8885c222d52dad741ec9a81f43
Zach Robinson <meandyouftw@gmail.com>
2022-09-15 11:01:43 -0700

    redearth palette updates

--- palmod/Game/Game_REDEARTH_A.cpp
+++ palmod/Game/Game_REDEARTH_A.cpp
@@ -59,7 +59,7 @@ CGame_REDEARTH_A::CGame_REDEARTH_A(uint32_t nConfirmedROMSize /* = -1 */, int nR
     //We need the proper unit amt before we init the main buffer
     m_nRedEarthMode = nRedEarthModeToLoad;
 
-    const uint32_t nSafeCountFor30 = 473;
+    const uint32_t nSafeCountFor30 = 615;
     const uint32_t nSafeCountFor31 = 1297;
     const uint32_t nSafeCountFor50 = 4;
 
@@ -613,10 +613,6 @@ BOOL CGame_REDEARTH_A::UpdatePalImg(int Node01, int Node02, int Node03, int Node
                         // The starting point is the absolute first palette for the sprite in question which is found in P1
                         nSrcStart -= nNodeIncrement;
                     }
-                }
-                else
-                {
-                    OutputDebugString(L"DANGER ZACH ROBINSON!\r\n");
                 }
             }
 
