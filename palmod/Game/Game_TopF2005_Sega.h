a208ad73ee54c836d749dbfcac95451fa1246c79
Zach Robinson <meandyouftw@gmail.com>
2022-09-30 12:57:20 -0700

    update sega mega drive games

--- palmod/Game/Game_TopF2005_Sega.h
+++ palmod/Game/Game_TopF2005_Sega.h
@@ -1,57 +1,41 @@
 #pragma once
-#include "gameclass.h"
+#include "GameClassByDir.h"
 #include "TOPF2005_SEGA_DEF.h"
-#include "..\extrafile.h"
 
-class CGame_TOPF2005_SEGA : public CGameWithExtrasFile
+class CGame_TOPF2005_SEGA : public CGameClassByDir
 {
 private:
-    static uint32_t m_nTotalPaletteCountForTOPF2005;
+    static inline const sDirectoryLoadingData m_sFileLoadingData =
+    {
+        {
+            { L"top fighter 2005 (unl).bin", 0x200000 },
+        },
+        FileReadType::Sequential,
+    };
 
-    static uint32_t rgExtraCountAll[TOPF2005_SEGA_NUMUNIT + 1];
-    static uint32_t rgExtraCountVisibleOnly[TOPF2005_SEGA_NUMUNIT + 1];
-    static uint32_t rgExtraLoc[TOPF2005_SEGA_NUMUNIT + 1];
-
-    static void InitializeStatics();
-    static uint32_t m_nConfirmedROMSize;
-
-    void LoadSpecificPaletteData(uint32_t nUnitId, uint32_t nPalId);
-    uint32_t GetPaletteCountForUnit(uint32_t nUnitId) override;
-
-    static constexpr auto EXTRA_FILENAME_TOPF2005 = L"TOPF2005e.txt";
-    static constexpr uint32_t m_nExpectedGameROMSize = 0x200000;
+    const sCoreGameData m_sCoreGameData
+    {
+        L"Top Fighter 2005 (Mega Drive)",
+        TOPF2005_SEGA,
+        IMGDAT_SECTION_SEGA,
+        TOPF2005_SEGA_IMGIDS_USED,
+        { NO_SPECIAL_OPTIONS, PALWriteOutputOptions::WRITE_16 },
+        eImageOutputSpriteDisplay::DISPLAY_SPRITES_LEFTTORIGHT,
+        DEF_BUTTONLABEL_NEOGEO,
+        AlphaMode::GameDoesNotUseAlpha,
+        ColMode::COLMODE_RGB333,
+        m_sFileLoadingData,
+        TOPF2005_UNITS,
+        ARRAYSIZE(TOPF2005_UNITS),
+        L"TOPF2005e.txt",               // Extra filename
+        32,                      // Count of palettes listed in the header
+        0x24798,                   // Lowest known location used for palettes
+    };
 
 public:
-    CGame_TOPF2005_SEGA(uint32_t nConfirmedROMSize);
-    ~CGame_TOPF2005_SEGA();
-
-    //Static functions / variables
-    static CDescTree MainDescTree;
-
-    static sDescTreeNode* InitDescTree();
-    static sFileRule GetRule(uint32_t nUnitId);
-
-    //Extra palette function
-    static uint32_t GetExtraCt(uint32_t nUnitId, BOOL fCountVisibleOnly = FALSE);
-    static uint32_t GetExtraLoc(uint32_t nUnitId);
-
-    //Normal functions
-    CDescTree* GetMainTree();
-
-    static uint32_t GetCollectionCountForUnit(uint32_t nUnitId);
-
-    // We don't fold these into one sDescTreeNode return because we need to handle the Extra section.
-    static uint32_t GetNodeCountForCollection(uint32_t nUnitId, uint32_t nCollectionId);
-    static LPCWSTR GetDescriptionForCollection(uint32_t nUnitId, uint32_t nCollectionId);
-    static const sGame_PaletteDataset* GetPaletteSet(uint32_t nUnitId, uint32_t nCollectionId);
-    static const sGame_PaletteDataset* GetSpecificPalette(uint32_t nUnitId, uint32_t nPaletteId);
-
-    uint32_t GetNodeSizeFromPaletteId(uint32_t nUnitId, uint32_t nPaletteId);
-    const sDescTreeNode* GetNodeFromPaletteId(uint32_t nUnitId, uint32_t nPaletteId, bool fReturnBasicNodesOnly);
-
-    BOOL UpdatePalImg(int Node01 = -1, int Node02 = -1, int Node03 = -1, int Node04 = -1);
+    CGame_TOPF2005_SEGA(uint32_t nConfirmedROMSize) { InitializeGame(nConfirmedROMSize, m_sCoreGameData); };
 
     uint32_t GetKnownCRC32DatasetsForGame(const sCRC32ValueSet** ppKnownROMSet = nullptr, bool* fNeedToValidateCRCs = nullptr) override;
 
-    static stExtraDef* TOPF2005_SEGA_EXTRA_CUSTOM;
+    static sFileRule GetRule(uint32_t nRuleId) { return CGameClassByDir::GetRule(nRuleId, m_sFileLoadingData); };
 };
