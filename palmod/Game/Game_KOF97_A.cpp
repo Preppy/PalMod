f57d032c591b6112f5c00602e724a8efee2e8116
Zach Robinson <meandyouftw@gmail.com>
2022-12-04 22:08:51 -0800

    Support for more KOF97 variants

--- palmod/Game/Game_KOF97_A.cpp
+++ palmod/Game/Game_KOF97_A.cpp
@@ -284,9 +284,28 @@ uint32_t CGame_KOF97_A::GetKnownCRC32DatasetsForGame(const sCRC32ValueSet** ppKn
     static sCRC32ValueSet knownROMs[] =
     {
         { L"King of Fighters '97 (Neo-Geo)", L"232-p2.sp2", 0x158b23f6, 0 },
+        { L"King of Fighters '97 (Neo-Geo)", L"232-p2.bin", 0x158b23f6, 0 },
         { L"King of Fighters '97 (Neo-Geo)", L"KOF97_p2.rom", 0x158b23f6, 0 },
-        { L"King of Fighters '97AE (Neo-Geo)", L"232ae.p2", 0x8cffe3f3, 0 },
         { L"King of Fighters '97 Plus (Neo-Geo bootleg)", L"kf97-p2p.bin", 0x5502b020, 0 },
+        
+        // larger size  kof97cn  232cn-p2.bin  King of Fighters '97 (10th Anniversary Chinese Edition, EGHT hack)
+        // shifted?     kof97inv 232inv-p2.sp2 King of Fighters '97 (Invincible Plus)
+        // split        kof97oro               King of Fighters '97 oroshi plus 2003
+
+        // kof97cbt 
+        { L"King of Fighters '97 - Combo Training (Hack, Ver. 2018)", L"97cbt-p2.bin", 0x91c0cfdb, 0 },
+        // kof97pls
+        { L"King of Fighters '97 Plus (bootleg)", L"kf97-p2p.bin", 0x5502b020, 0 },
+        // kof97ps
+        { L"King of Fighters '97 (Imitation Playstation final improved version 2016-10-29)", L"232-p2ps.sp2", 0x90723643, 0 },
+        // kof97rc
+        { L"King of Fighters '97 - Random Combo (Hack, Ver. 2010)", L"232-rc.sp2", 0x406e4a4b, 0 },
+        // kof97t
+        { L"King of Fighters '97 (Optimized Edition)", L"232t.p2", 0xd9e51750, 0 },
+        // kof97t2020
+        { L"King of Fighters '97 (Optimised Edition 2020)", L"232t2020-p2.sp2", 0x6d448462, 0 },
+        // kof97xt
+        { L"King of Fighters '97 - Final Battle (hack)", L"232xt-p2.bin", 0x6d4503ce, 0 },
     };
 
     if (ppKnownROMSet != nullptr)
@@ -307,7 +326,9 @@ uint32_t CGame_KOF97AE_A::GetKnownCRC32DatasetsForGame(const sCRC32ValueSet** pp
 {
     static sCRC32ValueSet knownROMs[] =
     {
+        { L"King of Fighters '97AE (Neo-Geo)", L"232ae.p2", 0x8cffe3f3, 0 },
         { L"King of Fighters '97 Anniversary Edition (Neo-Geo)", L"232ae-p2.sp2", 0x228aa8d1, 0 },
+        { L"King of Fighters '97 - Anniversary Edition (Fork Build 2.1.1811) [Hack]", L"232aef-p2.sp2", 0xf00ba90d, 0 },
     };
 
     if (ppKnownROMSet != nullptr)
