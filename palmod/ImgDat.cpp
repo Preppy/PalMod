07ff090d1f9dd0fdccb0d0c7ccf91616acf92bd7
Zach Robinson <meandyouftw@gmail.com>
2022-12-15 11:28:50 -0800

    preview updates: SFA3 and Red Earth

--- palmod/ImgDat.cpp
+++ palmod/ImgDat.cpp
@@ -297,9 +297,9 @@ void CImgDat::SanityCheckImgDat(ULONGLONG nFileSize, uint32_t nCurrentDatestamp,
         // not super critical for daily updates, but still useful
         const uint16_t nExpectedYear = 2022;
         const uint8_t nExpectedMonth = 12;
-        const uint8_t nExpectedDay = 11;
+        const uint8_t nExpectedDay = 15;
         const uint8_t nExpectedRevision = 0;
-        const ULONGLONG nExpectedFileSize = 211970633;
+        const ULONGLONG nExpectedFileSize = 215114627;
 
         const uint32_t nExpectedDatestamp = (nExpectedYear << 16) | (nExpectedMonth << 8) | (nExpectedDay);
 
