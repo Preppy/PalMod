0debd5e69bfe50fb1bfbeb5ee211a677c9897809
Zach Robinson <meandyouftw@gmail.com>
2023-10-08 09:13:05 -0700

    MK1A: genericize titling since we support all versions

--- palmod/Game/Game_MK1_A.cpp
+++ palmod/Game/Game_MK1_A.cpp
@@ -1,15 +1,15 @@
 #include "StdAfx.h"
 #include "Game_MK1_A.h"
 
-CGame_MK1_A::MK1ALoadingKey CGame_MK1_A::m_eVersionToLoad = MK1ALoadingKey::V50Normal;
+CGame_MK1_A::MK1ALoadingKey CGame_MK1_A::m_eVersionToLoad = MK1ALoadingKey::Base;
 
 void CGame_MK1_A::SetSpecialRuleForFileName(std::wstring strFileName)
 {
     const std::map<std::wstring, MK1ALoadingKey> m_rgFileNameToVersion =
     {
         // these should be all lower case
-        { L"l5_mortal_kombat_t-unit_uj12_game_rom.uj12", MK1ALoadingKey::V50Normal },
-        { L"mkt-uj12.bin", MK1ALoadingKey::V50Alt },
+        { L"l5_mortal_kombat_t-unit_uj12_game_rom.uj12", MK1ALoadingKey::V50Alt },
+        { L"mkt-uj12.bin", MK1ALoadingKey::Base },
     };
 
     CString strFileNameLowerCase = strFileName.c_str();
@@ -23,7 +23,7 @@ void CGame_MK1_A::SetSpecialRuleForFileName(std::wstring strFileName)
     }
     else
     {
-        m_eVersionToLoad = MK1ALoadingKey::V50Normal;
+        m_eVersionToLoad = MK1ALoadingKey::Base;
     }
 
     return;
@@ -33,9 +33,9 @@ CGame_MK1_A::CGame_MK1_A(uint32_t nConfirmedROMSize)
 {
     switch (m_eVersionToLoad)
     {
-    case MK1ALoadingKey::V50Normal:
+    case MK1ALoadingKey::Base:
     default:
-        InitializeGame(nConfirmedROMSize, m_sCoreGameData_V50T);
+        InitializeGame(nConfirmedROMSize, m_sCoreGameData_Base);
         break;
     case MK1ALoadingKey::V50Alt:
         InitializeGame(nConfirmedROMSize, m_sCoreGameData_V50TAlt);
@@ -47,9 +47,9 @@ sFileRule CGame_MK1_A::GetRule(uint32_t nRuleId)
 {
     switch (m_eVersionToLoad)
     {
-    case MK1ALoadingKey::V50Normal:
+    case MK1ALoadingKey::Base:
     default:
-        return CGameClassByDir::GetRule(nRuleId, m_sFileLoadingData_V50T);
+        return CGameClassByDir::GetRule(nRuleId, m_sFileLoadingData_Base);
     case MK1ALoadingKey::V50Alt:
         return CGameClassByDir::GetRule(nRuleId, m_sFileLoadingData_V50TAlt);
     }
--- palmod/Game/Game_MK1_A.h
+++ palmod/Game/Game_MK1_A.h
@@ -7,22 +7,55 @@ class CGame_MK1_A : public CGameClassByDir
 private:
     enum class MK1ALoadingKey
     {
-        V50Normal,
+        Base,
         V50Alt,
     };
 
     static MK1ALoadingKey m_eVersionToLoad;
 
-    static inline const sDirectoryLoadingData m_sFileLoadingData_V50T =
-    {
-        {
-            { L"l5_mortal_kombat_t-unit_uj12_game_rom.uj12", 0x80000 },
-            { L"l5_mortal_kombat_t-unit_ug12_game_rom.ug12", 0x80000 },
-        },
-        FileReadType::Interleaved_2FileSets,
-    };
+    // The palette data for MK1 is shared across all known game revisions.
+    // For hardware, MK uses the Midway Y Unit (Versions until 4.0) and
+    // Midway T Unit (Versions 4.0-5.0)
+    // 
+    // The checked revisions, and other file names that would differentiate them are:
+    // NiftyKombo666Hack
+    //  	kombo-u89.u89
+    //  	mortall_kombo_rom_u105-j4.u105.bin
+    // NiftyKomboHack
+    //  	nifty.105
+    //  	nifty.89
+    // PrototypeRev4-07-14-92
+    //		mkprot4.105
+    //		mkprot4.89
+    // PrototypeRev8-07-21-92
+    // 		mkprot8.105
+    // 		mkprot8.89
+    // PrototypeRev9-07-28-92
+    // 		mkprot9.105
+    // 		mkprot9.89
+    // Rev1.0-08-09-92
+    //  	mkg-u105.la1
+    //  	mkg-u89.la1
+    // Rev2.0-08-18-02
+    //  	mkg-u105.la2
+    //  	mkg-u89.la2
+    // Rev4.0-TUnit-02-11-93
+    // 	    mkr4ug12.bin
+    //  	mkr4uj12.bin
+    // Rev5.0TUnit
+    //  	sl1_mortal_kombat_u12_sound_rom.u12
+    //  	sl1_mortal_kombat_u13_sound_rom.u13
+    //  	sl1_mortal_kombat_u3_sound_rom.u3
+    // Fightcade's alternate Rev5.0TUnit zip:
+    // 	    <different base filenames>
+    // Turbo3.1-09-09-93Hack
+    //  	kombo-rom-u105.bin
+    //  	kombo-rom-u89.bin
+    // TurboNinjaTUnit-03-19-93Hack
+    //  	kombo-rom-ug-12.bin
+    //  	kombo-rom-uj-12.bin
 
-    static inline const sDirectoryLoadingData m_sFileLoadingData_V50TAlt =
+    static inline const sDirectoryLoadingData m_sFileLoadingData_Base =
     {
         {
             { L"mkt-uj12.bin", 0x80000 },
@@ -31,15 +64,24 @@ private:
         FileReadType::Interleaved_2FileSets,
     };
 
+    static inline const sDirectoryLoadingData m_sFileLoadingData_V50TAlt =
+    {
+        {
+            { L"l5_mortal_kombat_t-unit_uj12_game_rom.uj12", 0x80000 },
+            { L"l5_mortal_kombat_t-unit_ug12_game_rom.ug12", 0x80000 },
+        },
+        FileReadType::Interleaved_2FileSets,
+    };
+
     const std::vector<sCRC32ValueSet> m_rgCRC32Data =
     {
         { L"Mortal Kombat (rev 5.0 T-Unit 03/19/93)", L"l5_mortal_kombat_t-unit_uj12_game_rom.uj12", 0xf4990bf2, 0 },
-        { L"Mortal Kombat (rev 5.0 T-Unit 03/19/93)", L"mkt-uj12.bin", 0xf4990bf2, 0 },
+        { L"Mortal Kombat (Midway Arcade)", L"mkt-uj12.bin", 0xf4990bf2, 0 },
     };
 
-    const sCoreGameData m_sCoreGameData_V50T
+    const sCoreGameData m_sCoreGameData_Base
     {
-        L"Mortal Kombat (rev 5.0 T-Unit 03/19/93)",
+        L"Mortal Kombat (Midway Arcade)",
         MK1_A,
         IMGDAT_SECTION_OTHER,
         MK1_A_IMGIDS_USED,
@@ -48,7 +90,7 @@ private:
         DEF_BUTTONLABEL_2,
         AlphaMode::GameDoesNotUseAlpha,
         ColMode::COLMODE_RGB555_BE,
-        m_sFileLoadingData_V50T,
+        m_sFileLoadingData_Base,
         m_rgCRC32Data,
         MK1_A_UNITS,
         ARRAYSIZE(MK1_A_UNITS),
